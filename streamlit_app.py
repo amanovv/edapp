5f1d1b1eaeedd837ace14991b58c2256ae1c02d7
devo <devo@Dovrans-MacBook-Pro.local>
2021-07-11 11:52:46 -0400

    some changes xd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,24 @@
 import streamlit as st
 import os
-import urllib
+#import urllib
 import tensorflow as tf
 import pandas as pd 
 import cv2
 import time
+#import wget
 
 import numpy as np
 
+# downloading file requirs some work
+finished = 0
+while finished == 0:
+  st.spinner()
+  os.system(f"wget https://www.dropbox.com/s/072b5vf4b33bu1l/emotion_detection_model_for_streamlit.h5")
+  finished = 1
+#filename = wget.download(url)
 
-def download_file(file_path):
-    # Don't download the file twice. (If possible, verify the download using the file length.)
-  if os.path.exists(file_path):
-    if "size" not in EXTERNAL_DEPENDENCIES[file_path]:
-        return
-    elif os.path.getsize(file_path) == EXTERNAL_DEPENDENCIES[file_path]["size"]:
-        return
-
-  # These are handles to two visual elements to animate.
-  weights_warning, progress_bar = None, None
-  try:
-    weights_warning = st.warning("Downloading %s..." % file_path)
-    progress_bar = st.progress(0)
-    with open(file_path, "wb") as output_file:
-      with urllib.request.urlopen(EXTERNAL_DEPENDENCIES[file_path]["url"]) as response:
-        length = int(response.info()["Content-Length"])
-        counter = 0.0
-        MEGABYTES = 2.0 ** 20.0
-        while True:
-          data = response.read(8192)
-          if not data:
-              break
-          counter += len(data)
-          output_file.write(data)
-
-          # We perform animation by overwriting the elements.
-          weights_warning.warning("Downloading %s... (%6.2f/%6.2f MB)" %
-              (file_path, counter / MEGABYTES, length / MEGABYTES))
-          progress_bar.progress(min(counter / length, 1.0))
-
-  # Finally, we remove these visual elements by calling .empty().
-  finally:
-    if weights_warning is not None:
-      weights_warning.empty()
-    if progress_bar is not None:
-      progress_bar.empty()
-
-EXTERNAL_DEPENDENCIES = {
-    "emotion_detection_model_for_streamlit.h5": {
-    "url": "https://www.dropbox.com/s/072b5vf4b33bu1l/emotion_detection_model_for_streamlit.h5",
-    "size": 126265696
-    }
-}
-
+#filename = tf.keras.utils.get_file("emotion_detection_model_for_streamlit.h5", url)
 EMOTIONS = ['ANGRY', 'HAPPY', 'SAD', 'SURPRISE', 'NEUTRAL']
-
-# Download the the model file
-for filename in EXTERNAL_DEPENDENCIES.keys():
-  download_file(filename)
 
 st.title("Emotion Detector")
 st.header("This app detects your emotions! upload a picture to try it out!")
